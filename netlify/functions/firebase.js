9714764813c488a86edcf6bfd3128a39bc93d5a8
RickCrowley <rick.crowley@kellogg.northwestern.edu>
2021-03-04 21:10:37 -0600

    My attempt

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyB_gWlqXnj2x9AZ7sMgLdBpYjs0YlKg5HU",
+  authDomain: "kiei-451-b4e4b.firebaseapp.com",
+  projectId: "kiei-451-b4e4b",
+  storageBucket: "kiei-451-b4e4b.appspot.com",
+  messagingSenderId: "519361549325",
+  appId: "1:519361549325:web:d650889fef3f49e1e0b144"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
--- netlify/functions/like.js
+++ netlify/functions/like.js
@@ -4,6 +4,8 @@ let firebase = require('./firebase')
 exports.handler = async function(event) {
   let db = firebase.firestore()
 
+  
+
   // 🔥🔥🔥 Code-Along
   // Implement the like function
   // Step 2:  Parse out the like data, i.e. the event.body – pull out
@@ -11,17 +13,42 @@ exports.handler = async function(event) {
   //          in the POST request, and assign to variables. Use
   //          console.log if necessary, to ensure the values are what
   //          you're expecting.
+  // console.log(event.body)
+  let post = JSON.parse(event.body)
+  // console.log(post)
+  let postId = post.postId
+  let userId = post.userId
+
+  // console.log(`the post is ${postId}`)
+  // console.log(`the user is ${userId}`)
   // Step 3:  Check in the firestore collection to see if the user has
   //          already liked the post before adding a new like.
+  let querySnapshot = await db.collection(`likes`).where(`postId`,`==`, postId).where(`userId`, `==`, userId).get()
+  let numberOfLikes = querySnapshot.size
+  console.log(`number of likes: ${numberOfLikes}`)
   // Step 4:  If there's no likes with the given post id and user id,
   //          add the like to Firestore using the .add() function.
+  if (numberOfLikes == 0) {
+    await db.collection(`likes`).add({
+      postId: postId,
+      userId: userId
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({success:true})
+    }
+
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({sucess: false, error:`user already liked the post`})
+    }
+  }
   // Step 5:  Modify the return so that the body is different based on
   //          whether a new like was added or not. Any distinction is ok
   //          so long as the frontend will be able to identify which occurred.
   //          (optional) Use a relevant status code.
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({})
-  }
+  
 }
